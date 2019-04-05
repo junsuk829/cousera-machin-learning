38d58e6c7f2ec615d1ac95515f4b51a0a7891708
august.chang <august.chang@kakaocorp.com>
2019-04-05 12:20:40 +0900

    with regularzation

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,23 +41,21 @@ Theta_grad = zeros(size(Theta));
 %
 
 
-J = sum(sum(((X*Theta'-Y).^2).*R))/2
+J = sum(sum(((X*Theta'-Y).^2).*R))/2 +lambda/2*sum(sum(Theta.^2))+lambda/2*sum(sum(X.^2));
 
 for i=1:size(X,1)
 	idx = find(R(i,:)==1);
 	Theta_temp = Theta(idx,:);
 	Y_temp = Y(i,idx);
-	X_grad(i,:) = ( X(i,:)*Theta_temp' - Y_temp)*Theta_temp;
+	X_grad(i,:) = ( X(i,:)*Theta_temp' - Y_temp)*Theta_temp + lambda*X(i,:);
 
 end
 
 for i=1:size(Theta,1)
 	idx = find(R(:,i)==1);
-	%Theta_temp = Theta(i,idx);
 	Y_temp = Y(idx,i);
 	X_temp = X(idx,:)
-	Theta_grad(i,:) = ( X_temp*Theta(i,:)' - Y_temp)'*X_temp;
-
+	Theta_grad(i,:) = ( X_temp*Theta(i,:)' - Y_temp)'*X_temp + lambda*Theta(i,:);
 end
 
 
